30e583ed6c85fa317f5aad3e489d89871b90d1ee
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-09-21 15:49:09 +0530

    first Test

---
+++ src/test/java/com/testing/MathUtilsTest.java
@@ -0,0 +1,14 @@
+package com.testing;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+	void test() {
+		System.out.println("This test ran");
+	}
+
+}
